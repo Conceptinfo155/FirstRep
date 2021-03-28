10577abdf0c42a4bf8cba11a91e9577c89a003f4
conceptinfo155 <conceptinfo155@gmail.com>
2021-03-28 14:06:30 +0530

    Rally number: adding files

---
+++ RamJAVA/src/com/test/pack/System1.java
@@ -0,0 +1,9 @@
+package com.test.pack;
+
+public class System1 {
+	int a;
+	static String s;
+	static PrintStream1 out1=new  PrintStream1();
+	
+
+}
---
+++ RamJAVA/src/com/test/pack/Test.java
@@ -0,0 +1,10 @@
+package com.test.pack;
+
+public class Test {
+	public static void main(String[] args) {
+		System1.out1.println1("ramesh");
+		System1.out1.println1(10);
+		System1.out1.println1(true);
+	}
+
+}
