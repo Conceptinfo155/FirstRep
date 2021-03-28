8cd675c5d53c804049a90ff497667d6193d84955
conceptinfo155 <conceptinfo155@gmail.com>
2021-03-28 15:18:44 +0530

    modify

--- RamJAVA/src/com/test/pack/Test.java
+++ RamJAVA/src/com/test/pack/Test.java
@@ -5,6 +5,11 @@ public class Test {
 		System1.out1.println1("ramesh");
 		System1.out1.println1(10);
 		System1.out1.println1(true);
+		System.out.println("newLine");
+		System.out.println("=============");
+		System1.out1.println1(10);
+		System1.out1.println1(true);
+		System.out.println("newLine");
 	}
 
 }
